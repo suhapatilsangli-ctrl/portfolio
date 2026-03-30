05b9ceeb98ae1cbb312787280cf65d53edc05e72
suhapatilsangli-ctrl <suhapatil.sangli@gmail.com>
2026-03-30 23:56:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Portfolio Loaded");
+const elements = document.querySelectorAll(".fade-in");
+
+window.addEventListener("scroll", () => {
+    elements.forEach(el => {
+        const position = el.getBoundingClientRect().top;
+        if (position < window.innerHeight - 100) {
+            el.classList.add("show");
+        }
+    });
+});
+
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
